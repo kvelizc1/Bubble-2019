a04752f67119a0f7a44e167bffb2be8e38fc8457
kenyv <kenyv@DESKTOP-9ONC57S>
2019-08-09 15:38:35 -0600

    Primer laborario con Ing. Carlos (Ni siquera lo he terminado...) * Update: Ya es funcional. Contiene demasiados comentarios con versiones anteriores.

--- src/burbuja/Burbuja.java
+++ src/burbuja/Burbuja.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package burbuja;
 import javax.swing.JOptionPane;
 
@@ -24,6 +20,7 @@ public class Burbuja {
             arr[i]=Integer.parseInt(JOptionPane.showInputDialog(null,"Dame el numero "+aux));
             System.out.print(arr[i]+" ");//Verifico que mis numeros se hayan guardado
         }        
+  
         while(true){
             //el arreglo se comienza a verificar desde la pocision 1 y compara con la pocision previa, sea esta 0
             cambalache=false;
@@ -38,9 +35,23 @@ public class Burbuja {
             }
             if(cambalache==false)break;
         }//Voy a hacer esto hasta que ya no haya hecho ningun cambio
-        /*
-        Estaba utilizando un do while. Mi 
-        */
+
+/*
+       //ESTO NO FUNCIONA Y NO ENTIENDO POR QUE, SI DEBERIA FUNCIONAR IGUAL! >:'v
+        do{
+            //el arreglo se comienza a verificar desde la pocision 1 y compara con la pocision previa, sea esta 0
+            cambalache=false;
+            for(int i=1;i<arr.length;i++){ //Vamos a revisar cada pocision del arreglo
+                if(arr[i]<arr[i-1]){    //Si el valor que estoy verificando es menor que mi valor previo, vamor a hacer magia (cambio)
+                    aux=arr[i]; //"sacamos/copiamos" el valor desde el arreglo
+                    //Intercambiamos:
+                    arr[i]=arr[i-1]; //Mi valor menor lo asigno a la posicion previa
+                    arr[i-1]=aux;//"Pegamos" el vamor mayor a la posicion que estabamos evaluando
+                    cambalache=true;
+                }
+            }
+        }while(cambalache=false);//Voy a hacer esto hasta que ya no haya hecho ningun cambio
+ */
         System.out.println("\nValores ordenados: ");
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
